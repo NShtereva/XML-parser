67dd2791cbf6f836b02f24dc240f289e80ab9606
NShtereva <niki1shtereva@gmail.com>
2022-06-14 05:54:37 +0300

    Add class Parser

---
+++ parser.hpp
@@ -0,0 +1,17 @@
+#ifndef __PARSER_HPP
+#define __PARSER_HPP
+
+#include <iostream>
+
+class Parser
+{
+    private:
+        static const size_t MAX_LEN = 1000;
+
+        Parser() = default;
+
+    public:
+        static void useParser();
+};
+
+#endif
