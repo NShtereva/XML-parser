5343607b57f8378df25bdde6e745a0e2d1f5ee14
NShtereva <niki1shtereva@gmail.com>
2022-06-14 05:19:30 +0300

    Add class CommandParser

---
+++ commandParser.cpp
@@ -0,0 +1,154 @@
+#include "commandParser.hpp"
+
+#include <fstream>
+#include <cstring>
+#include <exception>
+
+CommandParser::CommandParser(const char* line)
+    : numberOfArgs(0)
+{
+    try
+    {
+        this->allocate();
+        this->processTheLine(line);
+    }
+    catch(const std::invalid_argument& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+CommandParser::CommandParser(const CommandParser& other)
+{
+    this->allocate();
+    for(int i = 0; i < other.numberOfArgs; i++)
+    {
+        strcpy(this->arguments[i], other.arguments[i]);
+    }
+    this->numberOfArgs = other.numberOfArgs;
+}
+
+CommandParser::~CommandParser()
+{
+    this->deallocate();
+}
+
+CommandParser& CommandParser::operator = (const CommandParser& other)
+{
+    if(this != &other)
+    {
+        this->deallocate();
+        this->allocate();
+
+        for(int i = 0; i < other.numberOfArgs; i++)
+        {
+            strcpy(this->arguments[i], other.arguments[i]);
+        }
+
+        this->numberOfArgs = other.numberOfArgs;
+    }
+    return *this;
+}
+
+void CommandParser::allocate()
+{
+    this->arguments = new(std::nothrow) char*[this->MAX_NUMBER_OF_ARGS];
+    if(!this->arguments)
+    {
+        std::cout << "Memory not allocated successfully!" << std::endl;
+        return;
+    }
+
+    for(int i = 0; i < this->MAX_NUMBER_OF_ARGS; i++)
+    {
+        this->arguments[i] = new(std::nothrow) char[this->MAX_LEN];
+        if(!this->arguments[i])
+        {
+            std::cout << "Memory not allocated successfully!" << std::endl;
+
+            delete[] this->arguments;
+            this->arguments = nullptr;
+
+            return;
+        }
+    }
+}
+
+void CommandParser::deallocate()
+{
+    for(int i = 0; i < this->MAX_NUMBER_OF_ARGS; i++)
+    {
+        delete[] this->arguments[i];
+    }
+    delete[] this->arguments;
+    this->arguments = nullptr;
+}
+
+bool CommandParser::isWhiteSpace(char a)
+{
+    return a == ' ' || a == '\t' || a == '\n';
+}
+
+void CommandParser::processTheLine(const char* line)
+{
+    int lineSize = strlen(line);
+
+    int i = 0, index = 0;
+    while(i < lineSize && index <= this->MAX_NUMBER_OF_ARGS)
+    {
+        this->arguments[index][0] = '\0';
+
+        while(isWhiteSpace(line[i]) && i < lineSize)
+        {
+            i++;
+        }
+
+        if(line[i] == '\"')
+        {
+            i++;
+
+            int j = 0;
+            while(line[i] != '\"' && i < lineSize)
+            {
+                this->arguments[index][j] = line[i];
+                i++; j++;
+            }
+            this->arguments[index][j] = '\0';
+
+            if(line[i] != '\"')
+                throw std::invalid_argument("Invalid input!");
+            else i++;
+
+            index++;
+            continue;
+        }
+
+        int j = 0;
+        while(!isWhiteSpace(line[i]) && i < lineSize)
+        {
+            this->arguments[index][j] = line[i];
+            i++; j++;
+        }
+        this->arguments[index][j] = '\0';
+
+        index++;
+    }
+
+    if(index > this->MAX_NUMBER_OF_ARGS)
+        throw std::invalid_argument("Invalid input!");
+
+    this->numberOfArgs = index;
+}
+
+size_t CommandParser::getNumberOfArguments() const
+{
+    return this->numberOfArgs;
+}
+
+const char* CommandParser::operator [] (int index) const
+{
+    if(index < 0 || index > this->MAX_NUMBER_OF_ARGS)
+        throw std::out_of_range("Invalid index!");
+
+    return this->arguments[index];
+}
---
+++ commandParser.hpp
@@ -0,0 +1,33 @@
+#ifndef __COMMANDPARSER_HPP
+#define __COMMANDPARSER_HPP
+
+#include <iostream>
+
+class CommandParser
+{
+	private:
+		static const size_t MAX_NUMBER_OF_ARGS = 10; 
+		static const size_t MAX_LEN = 255; 
+
+		char** arguments;
+		size_t numberOfArgs;
+
+		void allocate();
+		void deallocate();
+
+		static bool isWhiteSpace(char a);
+		void processTheLine(const char* line);
+
+	public:
+		CommandParser(const char* line);
+        CommandParser(const CommandParser& other);
+        ~CommandParser();
+
+        CommandParser& operator = (const CommandParser& other);
+
+		size_t getNumberOfArguments() const;
+
+		const char* operator [] (int index) const;
+};
+
+#endif
